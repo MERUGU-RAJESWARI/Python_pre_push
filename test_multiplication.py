61d1d18147ef45bdfed93e82f9404e15d31df45e
MERUGU-RAJESWARI <139354752+MERUGU-RAJESWARI@users.noreply.github.com>
2025-03-11 11:35:15 +0530

    Create test_multiplication.py

---
+++ test_multiplication.py
@@ -0,0 +1,16 @@
+import unittest
+from multiplication import calculate_multiplication
+
+class TestMultiplication(unittest.TestCase):
+    def test_valid_multiplication(self):
+        self.assertEqual(calculate_multiplication(2, 3), 6)
+        self.assertEqual(calculate_multiplication(-1, 5), -5)
+
+    def test_invalid_inputs(self):
+        with self.assertRaises(TypeError):
+            calculate_multiplication("a", 2)
+        with self.assertRaises(TypeError):
+            calculate_multiplication(3, "b")
+
+if __name__ == "__main__":
+    unittest.main()
